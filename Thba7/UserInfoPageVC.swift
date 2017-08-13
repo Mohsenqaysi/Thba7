e6c6f389d5ed35eca4455cfd28fc00a784072937
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2017-08-13 17:09:32 +0100

    Clean up the code on the user order class

--- Thba7/UserInfoPageVC.swift
+++ Thba7/UserInfoPageVC.swift
@@ -17,9 +17,10 @@ private struct OrderInfo {
     var countity: String?
 }
 
-class UserInfoPageVC: UIViewController, UITextFieldDelegate {
+class UserInfoPageVC: UIViewController {
     
     var passedOrderData = [String]()
+    let userInfoViewContinerHeight: CGFloat = 100.0
     
     let scrollingView: UIScrollView = {
         let v = UIScrollView()
@@ -32,21 +33,21 @@ class UserInfoPageVC: UIViewController, UITextFieldDelegate {
         return v
     }()
     
-    let redView: UIView = {
+    let userInfoViewContiner: UIView = {
         let v = UIView()
         v.backgroundColor = .white
         v.viewCardThemeWithCornerRadius(radius: 7)
         return v
     }()
     
-    let blueView: UIView = {
+    let orderInfoViewContiner: UIView = {
         let v = UIView()
         v.backgroundColor = .blue
         v.viewCardThemeWithCornerRadius(radius: 7)
         return v
     }()
     
-    let greenView: UIView = {
+    let locationOnMapInfoContinerView: UIView = {
         let v = UIView()
         v.backgroundColor = .green
         v.viewCardThemeWithCornerRadius(radius: 7)
@@ -57,6 +58,7 @@ class UserInfoPageVC: UIViewController, UITextFieldDelegate {
         let v = UITextField()
         v.placeholder = "الأسم (*)"
         v.keyboardType = .asciiCapable
+        v.returnKeyType = .done
         v.textAlignment = .right
         return v
     }()
@@ -65,6 +67,7 @@ class UserInfoPageVC: UIViewController, UITextFieldDelegate {
         let v = UITextField()
         v.placeholder = "العنوان (*)"
         v.keyboardType = .asciiCapable
+        v.returnKeyType = .done
         v.textAlignment = .right
         return v
     }()
@@ -73,8 +76,17 @@ class UserInfoPageVC: UIViewController, UITextFieldDelegate {
         let v = UITextField()
         v.placeholder = "رقم الجوال (*)"
         v.keyboardType = .numberPad
+        //        v.adjustsFontSizeToFitWidth = true
         v.textAlignment = .right
         return v
+    }()
+    
+    let toolBarAndDoneButtonUserNumber: UIToolbar = {
+        let toolBarAndDoneButtonUserNumber = UIToolbar()
+        toolBarAndDoneButtonUserNumber.sizeToFit()
+        let barBtnDone = UIBarButtonItem(title: "تم", style: .done, target: self, action: #selector(handeluserPhoneNumberDoneButton))
+        toolBarAndDoneButtonUserNumber.setItems([barBtnDone], animated: true) // You can even add cancel button too
+        return toolBarAndDoneButtonUserNumber
     }()
     
     let userNameSepearator: UIView = {
@@ -110,43 +122,70 @@ class UserInfoPageVC: UIViewController, UITextFieldDelegate {
         print("--------------------------")
         print(passedOrderData)
         print("--------------------------")
-//        self.userName.delegate = self
-        // add the scrollingView inside the View
+        scrollViewContiner()
+    }
+    func scrollViewContiner() {
+        // Add the scrollingView inside the View
         view.addSubview(scrollingView) // add to rootView
         scrollingView.addSubview(deliveryInfoLabel)
-        scrollingView.addSubview(redView) // Red
-        redView.addSubview(userName) // userName Insdie RedView
-        redView.addSubview(userNameSepearator) // lineSepearator
-        redView.addSubview(userAddress) // userAddress Insdie RedView
-        redView.addSubview(userAddressSepearator) // lineSepearator
-        redView.addSubview(userPhoneNumber) // userPhoneNumber Insdie RedView
-//        redView.addSubview(userPhoneNumberSepearator) // lineSepearator
-        scrollingView.addSubview(blueView) // Blue
-        scrollingView.addSubview(greenView) // Green
-        
-        
-        guard let navSize = self.navigationController?.navigationBar.frame.size.height else {return}
-        
-        let redViewHeight: CGFloat = 100.0
+        scrollingView.addSubview(userInfoViewContiner) // userInfoContinerView
         
         _ = scrollingView.anchorWithConstantsToTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 5 , leftConstant: 0, bottomConstant: 0, rightConstant: 0)
-        _ = deliveryInfoLabel.anchor(top: scrollingView.topAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0,widthConstant: 0, heightConstant: redViewHeight / 3)
+        _ = deliveryInfoLabel.anchor(top: scrollingView.topAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0,widthConstant: 0, heightConstant: userInfoViewContinerHeight / 3)
         
-        _ = redView.anchor(top: deliveryInfoLabel.bottomAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: redViewHeight)
+        _ = userInfoViewContiner.anchor(top: deliveryInfoLabel.bottomAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: userInfoViewContinerHeight)
+        
+        // MARK: Add other Subviews
+        userInfoContinerView()
+        orderInfoContinerView()
+        userLocationOnMapInfoContinerView()
+        
+    }
+    
+    func userInfoContinerView() {
+        // MARK: Add views to the ContinerView
+        userInfoViewContiner.addSubview(userName) // userName Insdie userInfoViewContiner
+        userInfoViewContiner.addSubview(userNameSepearator) // lineSepearator
+        userInfoViewContiner.addSubview(userAddress) // userAddress Insdie userInfoViewContiner
+        userInfoViewContiner.addSubview(userAddressSepearator) // lineSepearator
+        userInfoViewContiner.addSubview(userPhoneNumber) // userPhoneNumber Insdie userInfoViewContiner
+        
+        self.userName.delegate = self
+        self.userAddress.delegate = self
+        self.userPhoneNumber.delegate = self
         
         // name
-        _ = userName.anchor(top: redView.topAnchor, left: redView.leftAnchor, bottom: nil, right: redView.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: redViewHeight / 3)
-        _ = userNameSepearator.anchor(top: userName.bottomAnchor, left: redView.leftAnchor, bottom: nil, right: redView.rightAnchor, topConstant: 1, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 1)
+        _ = userName.anchor(top: userInfoViewContiner.topAnchor, left: userInfoViewContiner.leftAnchor, bottom: nil, right: userInfoViewContiner.rightAnchor, topConstant: 1, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: userInfoViewContinerHeight / 3)
+        _ = userNameSepearator.anchor(top: userName.bottomAnchor, left: userInfoViewContiner.leftAnchor, bottom: nil, right: userInfoViewContiner.rightAnchor, topConstant: 1, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 1)
         // Address
-        _ = userAddress.anchor(top: userName.bottomAnchor, left: redView.leftAnchor, bottom: nil, right: redView.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: redViewHeight / 3)
-        _ = userAddressSepearator.anchor(top: userAddress.bottomAnchor, left: redView.leftAnchor, bottom: nil, right: redView.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 1)
+        _ = userAddress.anchor(top: userName.bottomAnchor, left: userInfoViewContiner.leftAnchor, bottom: nil, right: userInfoViewContiner.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: userInfoViewContinerHeight / 3)
+        _ = userAddressSepearator.anchor(top: userAddress.bottomAnchor, left: userInfoViewContiner.leftAnchor, bottom: nil, right: userInfoViewContiner.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 1)
         // Phone Number
-        _ = userPhoneNumber.anchor(top: userAddress.bottomAnchor, left: redView.leftAnchor, bottom: nil, right: redView.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: redViewHeight / 3)
-//        _ = userPhoneNumberSepearator.anchor(top: userPhoneNumber.bottomAnchor, left: redView.leftAnchor, bottom: redView.bottomAnchor, right: redView.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 1)
+        _ = userPhoneNumber.anchor(top: userAddress.bottomAnchor, left: userInfoViewContiner.leftAnchor, bottom: nil, right: userInfoViewContiner.rightAnchor, topConstant: 2, leftConstant: 0, bottomConstant: 3, rightConstant: 12, widthConstant: 0, heightConstant: userInfoViewContinerHeight / 4)
         
-        _ = blueView.anchor(top: redView.bottomAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: CGFloat().getScreenWidth(), heightConstant: 400)
-        
-        _ = greenView.anchor(top: blueView.bottomAnchor, left: scrollingView.leftAnchor, bottom: scrollingView.bottomAnchor, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: CGFloat().getScreenWidth(), heightConstant: 400)
-        
+        //Add done button to userPhoneNumber pad keyboard
+        userPhoneNumber.inputAccessoryView = toolBarAndDoneButtonUserNumber
+    }
+    
+    func handeluserPhoneNumberDoneButton()  {
+        userPhoneNumber.endEditing(true)
+    }
+    
+    func orderInfoContinerView() {
+        scrollingView.addSubview(orderInfoViewContiner) // Blue
+        _ = orderInfoViewContiner.anchor(top: userInfoViewContiner.bottomAnchor, left: scrollingView.leftAnchor, bottom: nil, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: CGFloat().getScreenWidth(), heightConstant: 400)
+    }
+    
+    func userLocationOnMapInfoContinerView() {
+        scrollingView.addSubview(locationOnMapInfoContinerView) // Green
+        _ = locationOnMapInfoContinerView.anchor(top: orderInfoViewContiner.bottomAnchor, left: scrollingView.leftAnchor, bottom: scrollingView.bottomAnchor, right: scrollingView.rightAnchor, topConstant: 5, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: CGFloat().getScreenWidth(), heightConstant: 400)
+    }
+}
+
+extension UserInfoPageVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return false
     }
 }
